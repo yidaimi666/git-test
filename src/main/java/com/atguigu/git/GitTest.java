0f423256c8a9d316d6ec75fccfaa885bd6a91ce7
weilai <2571505784@qq.com>
2022-10-20 15:16:32 +0800

    third commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello niubiniubi");
         System.out.println("hello niubiniubi2");
+        System.out.println("hello niubiniubi3");
     }
 }
