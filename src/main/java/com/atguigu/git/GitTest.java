f59331cf7c2e51150962be3f89d9bb182ccd13d2
weilai <2571505784@qq.com>
2022-10-20 15:30:25 +0800

    master commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello niubiniubi2");
         System.out.println("hello niubiniubi3");
         System.out.println("hello niubiniubi4");
+        System.out.println("master test!");
     }
 }
